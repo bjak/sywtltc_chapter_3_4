1a66fef5bd9eb4b3c8cbdf33ebad7970b5666cf8
bjak <blake_jakobsson@utexas.edu>
2018-04-16 22:40:20 -0500

    Added Test for Refund Function

--- test_code.py
+++ test_code.py
@@ -7,6 +7,18 @@ def test_revised_code():
 
     revised_code.purchase("Xmen 8: The Xmennening", "Ricci")
 
-    assert revised_code.MOVIE_DB["Xmen 8: The Xmennening"] == 9
+    assert revised_code.MOVIE_DB["Xmen 8: The Xmennening"] == 10
     assert revised_code.MOVIEGOERS["Ricci"]["cash"] == 4
     assert revised_code.MOVIEGOERS["Ricci"]["tickets"] == 0
+
+    revised_code.purchase("The Bromance", "Cary")
+
+    assert revised_code.MOVIE_DB["The Bromance"] == 19
+    assert revised_code.MOVIEGOERS["Cary"]["cash"] == 115.0
+    assert revised_code.MOVIEGOERS["Cary"]["tickets"] == 1
+
+    revised_code.refund("The Bromance", "Cary")
+
+    assert revised_code.MOVIE_DB["The Bromance"] == 20
+    assert revised_code.MOVIEGOERS["Cary"]["cash"] == 120.0
+    assert revised_code.MOVIEGOERS["Cary"]["tickets"] == 0
