ac409ced1e98089d29db26af089e2f00a7899817
bjak <blake_jakobsson@utexas.edu>
2018-04-18 09:48:38 -0500

    Broke out functions for Revised Code

--- revised_code.py
+++ revised_code.py
@@ -1,39 +1,101 @@
 """This code is for a Miovie Ticketing System"""
 
-CASH = 5.0
-MOVIE_DB = {"Xmen 8: The Xmennening": 10, "The Bromance" : 20,
-            "Gigli: The Play: The Book: The movie" : 102}
-MOVIEGOERS = {"Bob" : {"movies":[], "cash":100.0, "tickets": 0},
-              "Jim" : {"movies": ["Xmen 8: The Xmennening"], "cash":10.0, "tickets":0},
-              "Cary" : {"movies": ["Gigli: The Play: The Book: The movie",
-                                   "The Bromance"], "cash":120.0, "tickets":0},
-              "Ricci": {"movies": [], "cash":4.0, "tickets":0}}
+from movie_database import MOVIE_DB, MOVIEGOERS, CASH
 
-def purchase(movie, moviegoer):
-    """Purchase Ticket Function"""
+def check_cash(moviegoer):
+    """Check to see if moviegoer has enough cash"""
 
-    if MOVIEGOERS[moviegoer]["cash"] > 5.0: #Check if Moviegoer has enough cash
-
-        MOVIE_DB[movie] = MOVIE_DB[movie] - 1
-        MOVIEGOERS[moviegoer]["cash"] = MOVIEGOERS[moviegoer]["cash"] - 5.0
-        MOVIEGOERS[moviegoer]["movies"].append(movie)
-        MOVIEGOERS[moviegoer]["tickets"] = MOVIEGOERS[moviegoer]["tickets"] + 1
-
+    if MOVIEGOERS[moviegoer]["cash"] > 5.0:
+        
         return True
 
-def refund(movie, moviegoer):
-    """Refund Ticket function"""
+    else:
 
-    if purchase == True:
+        print("You do not have enough cash")
+
+def take_cash(moviegoer):
+    """Take Cash for Movie from Moviegoer"""
+    
+    if check_cash() == True:
+
+        MOVIEGOERS[moviegoer]["cash"] = MOVIEGOERS[moviegoer]["cash"] - 5.0 
+
+def movie_append(moviegoer, movie):
+    """Append Movie"""
+    
+    if check_cash() == True:
+
+        MOVIEGOERS[moviegoer]["movies"].append(movie)
+
+def remove_seat(movie):
+    """Remove Seat from Movie Database"""
+
+    if check_cash() == True:
+
+        MOVIE_DB[movie] = MOVIE_DB[movie] - 1
+
+def give_ticket(moviegoer):
+    """Remove Seat from Movie database"""
+
+    if check_cash() == True:
+    
+        MOVIEGOERS[moviegoer]["tickets"] = MOVIEGOERS[moveigoer]["tickets"] + 1
+
+def check_ticket(moviegoer):
+    """Check if moviegoer has ticket before refund"""
+
+    if MOVIEGOERS[moviegoer]["tickets"] > 0:
+
+        return True 
+
+def refund_cash(moviegoer):
+    """Refund Cash Function"""
+
+    if check_ticket == True:
+
+        MOVIEGOERS[moviegoer]["cash"] = MOVIEGOERS[moviegoer]["cash"] + 5.0
+
+def movie_remove(movie, moviegoer):
+    """Remove Movie"""
+
+    if check_ticket == True:
+        
+        MOVIEGOERS[moviegoer]["movies"].remove(movie)
+
+def add_seat(movie):
+    """Add Seat back to Movie database"""
+    
+    if check_ticket == True:
 
         MOVIE_DB[movie] = MOVIE_DB[movie] + 1
-        MOVIEGOERS[moviegoer]["cash"] = MOVIEGOERS[moviegoer]["cash"] + 5.0
-        MOVIEGOERS[moviegoer]["movies"].append(movie)
-        MOVIEGOERS[moviegoer]["tickets"] = MOVIEGOERS[moviegoer]["tickets"] - 1
 
-def ticket_consume(moviegoer):
-    """Consume the purchase ticket"""
-    
-    if purchase == True:
+def refund_ticket():
+    """Take Ticket back from Moviegoer"""
 
-        MOVIEGOERS[moviegoer]["tickets"] = MOVIEGOERS[moviegoer]["tickets"] - 1
+    if check_ticket == True:
+
+        MOVIEGOERS[moviegoer]["tickets"] = MOVIERGOERS[moviegoer]["tickets"] - 1
+
+def purchase_ticket(movie, moviegoer):
+    """Purchase Ticker Function"""
+
+    check_cash(moviegoer)
+    take_cash(moviegoer)
+    movie_append(movie, moviegoer)
+    remove_seat(movie)
+    give_ticket(moviegoer)
+
+def refund_ticket(movie, moviegoer):
+
+    check_ticket(moviegoer)
+    refund_cash(moviegoer)
+    movie_remove(movie, moviegoer)
+    add_seat(movie)
+    refund_ticket(moviegoer)
+
+#def ticket_consume(moviegoer):
+ #   """Consume the purchase ticket"""
+  #  
+   # if purchase == True:
+
+    #    MOVIEGOERS[moviegoer]["tickets"] = MOVIEGOERS[moviegoer]["tickets"] - 1
